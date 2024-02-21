348a34b37cb3d2d9e35cc5b22991c406e3760270
Nikhil's Companion <edu.tiwarinikhil@gmail.com>
2024-02-21 23:50:29 +0530

    updated homepage

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Hero() {
+    return (
+        <>
+
+            <section
+                className="relative  bg-[url(https://plus.unsplash.com/premium_photo-1663100244280-063bffeccd1e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center bg-no-repeat"
+            >
+                <div
+                    className="absolute  backdrop-blur-sm inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
+                ></div>
+
+                <div
+                    className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:w-screen lg:items-center lg:px-8"
+                >
+                    <div className="max-w-xl text-justify ltr:sm:text-left rtl:sm:text-right">
+                        <h1 className="text-4xl font-extrabold text-white">
+                            Let's Launch Your Dream.
+
+                            <strong className="block font-extrabold text-black  bg-yellow-300/80"> Crowdfund your passion, spark innovation. </strong>
+                        </h1>
+
+                        <p className="mt-8 mb-0 text-2xl text-white">
+                            Unleash your ideas, ignite support, and make an impact. Our platform empowers you to create & share your campaign with the world.  <br /><br />
+                        </p>
+                        <p className='text-2xl  font-bold'>
+                            Explore inspiring campaigns or launch your own together, we can make anything possible.
+                        </p>
+
+                        <div className="mt-8 flex flex-wrap gap-4 text-center">
+                            <Link className="self-center p-4 px-[3em] border-4 rounded-xl hover:bg-sky-500 bg-orange-500" to={'/CreateCampaign'}>Create Your Campaign !</Link>
+
+                            <Link className="self-center p-4 px-[4em] border-4 rounded-xl transition bg-gray-200 hover:bg-sky-500" to={'/JoinCampaign'}>Explore Campaigns </Link>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Hero
---
+++ client/src/components/WhyUS.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+function WhyUS() {
+    return (
+        <>
+            <span className="relative flex justify-center mt-8">
+                <div
+                    className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"
+                ></div>
+
+                <span className="relative z-10 bg-white px-6">Why choose OFI</span>
+            </span>
+
+            <section>
+                <div className="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
+                    <div className="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
+                        <div className="relative z-10 lg:py-16">
+                            <div className="relative h-64 sm:h-80 lg:h-full">
+                                <img
+                                    alt="blockchain technology"
+                                    src="https://images.unsplash.com/photo-1639762681485-074b7f938ba0?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                    className="absolute inset-0 h-full w-full object-cover"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="relative flex items-center bg-gray-100">
+                            <span
+                                className="hidden lg:absolute lg:inset-y-0 lg:-start-16 lg:block lg:w-16 lg:bg-gray-100"
+                            ></span>
+
+                            <div className="p-8 sm:p-16 lg:p-24">
+                                <h2 className="text-3xl mb-4 font-bold sm:text-3xl">
+                                    Organise your croudfunding with Our Secure and Transparent Platform
+                                </h2>
+
+                                <p className="mt-4 text-xl text-gray-600">
+                                    Our platform is not only is it free to use, but it's also open-source, ensuring transparency and accountability at every step.
+                                </p>
+                                <p className="mt-4 text-xl text-gray-600">
+                                    Powered by Web3 technology, our platform offers unrivaled security, safeguarding your data and transactions with the highest standards of encryption
+                                </p>
+                                <a
+                                    href="/#services"
+                                    className="mt-8 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                                >
+                                    See Services
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default WhyUS
