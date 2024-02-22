6c5bf4a20ee5991c742cfa259998cb2fb875b4a7
Yashasvi Sharma <142658623+Yashasvi213@users.noreply.github.com>
2024-02-22 22:23:18 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,21 @@ import creatorAnimation from "../assets/creatorAnimation.json";
 import donateboxAnimation from "../assets/donateboxAnimation.json";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+<<<<<<< main
+import Hero from "../components/Hero";
+import WhyUS from "../components/WhyUS";
+import Services from "../components/Services";
+function Home() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Hero />
+      {/* <div className=" w-[100vw] h-[40vh]">
+    <div className= 'pl-10 flex w-[90%] justify-between'> 
+    <div>
+     <h1 className="text-[2.5em]">Create your own Campaign</h1>
+=======
+>>>>>>> main
 
 import campaignImage from "../assets/campaign.png"; // Import your campaign image
 import exploreImage from "../assets/explore.png"; // Import your explore image
@@ -70,7 +85,16 @@ function Home() {
           </div>
         </div>
       </div>
+<<<<<<< main
+      <Link className="self-center p-4 px-[4em] border-4 rounded-xl bg-orange-500" to={'/JoinCampaign'}>Explore Campaigns </Link>
+    
+    </div>    
+   </div> */}
+      <WhyUS />
+    </>
+=======
     </div>
+>>>>>>> main
   );
 }
 
